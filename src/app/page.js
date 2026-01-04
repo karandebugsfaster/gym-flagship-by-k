52a21f26a756af745d61992cd459cb7606ab73d6
Karan Maurya <karandebugs@gmail.com>
2026-01-04 20:36:48 +0530

    day 1

--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,22 @@ export default function Home() {
   ];
   const [activeSection, setActiveSection] = useState("Home");
   const [mounted, setMounted] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  useEffect(() => {
+    if (!mounted) return;
+
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 80);
+    };
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [mounted]);
 
   useEffect(() => {
     setMounted(true);
@@ -56,7 +72,12 @@ export default function Home() {
         fill
         priority
         quality={90}
-        className="object-cover object-[80%_50%] -z-10"
+        className="
+        object-cover
+        object-[60%_50%]
+        sm:object-[70%_50%]
+        lg:object-[80%_50%] 
+        -z-10"
       />
 
       {/* ✅ ADDED: Overlay for readability */}
@@ -74,9 +95,13 @@ export default function Home() {
         </ul>
       </div>
 
-      <div className="sticky top-0 z-50 h-[74px] flex items-center justify-around">
+      <div
+        className={`sticky top-0 z-50 h-[74px] flex items-center justify-around transition-all duration-300 ease-out
+        ${scrolled ? "bg-black/50 backdrop-blur shadow-md" : "bg-transparent"}
+      `}
+      >
         <div className="font-bold text-2xl text-white flex items-center gap-2">
-          <div>
+          <div className="relative z-10">
             <Image
               src="/images/real-world-2.png"
               alt="Real World Logo"
@@ -85,7 +110,7 @@ export default function Home() {
               className="mix-blend-multiply"
             />
           </div>
-          <a href="#Home" className="scroll-mt-[111px">The Real World</a>
+          <a href="#Home">The Real World</a>
         </div>
         <div>
           <ul className="flex gap-3">
@@ -95,6 +120,7 @@ export default function Home() {
                   whileHover={{ scale: 1.1 }}
                   animate={{ scale: activeSection === menu ? 1.1 : 1 }}
                   transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                  className="hidden lg:flex"
                   key={i}
                 >
                   {mounted && (
@@ -115,20 +141,120 @@ export default function Home() {
             })}
           </ul>
         </div>
-        <div className="font-bold text-2xl text-white">Log In</div>
+        <div className="flex items-center justify-center gap-1 md:gap-4 lg:gap-1">
+          <div className="relative z-10">
+            <Image
+              src="/images/avatar-loop.gif"
+              alt="Avatar"
+              width={40}
+              height={40}
+              className="mix-blend-multiply"
+            />
+          </div>
+          <div className="relative z-10 lg:hidden">
+            <button
+              className="cursor-pointer"
+              onClick={() => {
+                setToggleMenu(!toggleMenu);
+              }}
+            >
+              <Image
+                src="/images/hamburger-white.svg"
+                alt="Avatar"
+                width={40}
+                height={40}
+                className="mix-blend-multiply "
+              />
+            </button>
+          </div>
+          <motion.div
+            className="font-bold text-l hidden lg:flex rounded-full p-3 px-5 bg-[#a5a5a5] text-black hover:bg-orange-500 hover:text-white"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 300, damping: 20 }}
+          >
+            SIGN-UP
+          </motion.div>
+        </div>
       </div>
+      {/* BACKDROP */}
+      {toggleMenu && (
+        <div
+          onClick={() => setToggleMenu(false)}
+          className="fixed inset-0 bg-black/40 backdrop-blur-sm z-40 lg:hidden"
+        />
+      )}
+
+      {/* MOBILE MENU */}
+      <motion.div
+        initial={{ x: "100%" }}
+        animate={{ x: toggleMenu ? "0%" : "100%" }}
+        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+        className="
+         fixed top-0 right-0 h-screen w-[70%] sm:w-[50%]
+         bg-black/70 backdrop-blur
+         z-50
+         flex flex-col gap-6
+         pt-28 px-6
+         lg:hidden"
+      >
+        <button
+          onClick={() => setToggleMenu(false)}
+          className="absolute top-6 right-6 text-white text-3xl font-bold"
+        >
+          ✕
+        </button>
+
+        {/* {menus.map((menu, i) => (
+          <a
+            key={i}
+            href={`#${menu}`}
+            onClick={() => setToggleMenu(false)}
+            className="text-white text-lg font-bold hover:text-orange-500"
+          >
+            {menu}
+          </a>
+        ))} */}
+        <ul className="flex flex-col py-1 gap-10">
+          {menus.map((menu, i) => {
+            return (
+              <motion.li
+                whileHover={{ scale: 1.1 }}
+                animate={{ scale: activeSection === menu ? 1.1 : 1 }}
+                transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                key={i}
+              >
+                {mounted && (
+                  <a
+                    href={`#${menu}`}
+                    className={`rounded-full p-3 px-5 font-bold transition hover:bg-orange-500 hover:text-white
+                        ${
+                          activeSection === menu
+                            ? "bg-orange-500 text-white"
+                            : "bg-[#a5a5a5] text-black"
+                        }`}
+                  >
+                    {menu}
+                  </a>
+                )}
+              </motion.li>
+            );
+          })}
+        </ul>
+      </motion.div>
       <div>
         <section
           id="Home"
-          className="h-screen flex flex-col items-start justify-center gap-4 p-28"
+          className="h-screen flex flex-col items-start justify-end-safe lg:justify-center pb-[189px] md:pb-[250px] lg:pb-0 gap-4 sm:gap-5 md:gap-6 p-6 sm:p-10 md:p-16 lg:p-28 scroll-mt-[111px]"
         >
           <h1
             className="
-            text-7xl 
+            text-4xl sm:text-5xl md:text-6xl lg:text-7xl 
             font-bold 
             bg-[linear-gradient(90deg,rgba(254,163,0,1)_0%,rgba(245,122,4,1)_55%,rgba(236,79,9,1)_100%)]
             bg-clip-text 
-            text-transparent"
+            text-transparent
+            "
           >
             LESS TALK. <br /> MORE WORK.
           </h1>
